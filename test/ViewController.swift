2c8e92bdd078f5323651688db659f424f9001179
deejayfaki <fox73relax@gmail.com>
2022-07-18 12:50:35 +0400

    create project

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Станислав Терентьев on 18.07.2022.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
